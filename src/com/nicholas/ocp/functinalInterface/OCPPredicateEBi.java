41d6e209f2cdf5be57c218c5312430f8ce79e0a1
Nicholas-Barbosa <nicholas.henrique0604@hotmail.com>
2020-11-04 01:22:12 -0300

    Metodos default das interfcaes padroes

--- src/com/nicholas/ocp/functinalInterface/OCPPredicateEBi.java
+++ src/com/nicholas/ocp/functinalInterface/OCPPredicateEBi.java
@@ -1,6 +1,7 @@
 package com.nicholas.ocp.functinalInterface;
 
 import java.util.function.BiPredicate;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class OCPPredicateEBi {
@@ -18,6 +19,22 @@ public class OCPPredicateEBi {
 
 		BiPredicate<String, String> bp = String::startsWith;
 		System.out.println(bp.test("nihoals", "n"));
-		
+
+		Predicate<String> p1 = s -> s.startsWith("n");
+		Predicate<String> p2 = s -> s.endsWith("s");
+		Predicate<String> together = p1.and(p2).negate();
+		System.out.println(together.test("n"));
+
+		Function<Integer, Integer> f1 = s -> s * 10;
+		Function<Integer, Integer> f2 = s -> s + 0;
+		Function<Integer, Integer> funTogether = f2.compose(f1);
+		System.out.println(funTogether.apply(10));
+
+		/*
+		 * Retorno do compose da functiona dentro do compose, serve como parametro de
+		 * entrada pra quem ta chamando
+		 * 
+		 * 
+		 */
 	}
 }
