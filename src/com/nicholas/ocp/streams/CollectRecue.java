9e2bf6d954710d65c719d478154eec1efc027d6f
Nicholas-Barbosa <nicholas.henrique0604@hotmail.com>
2020-11-09 15:27:47 -0300

    Collectors with streams

--- src/com/nicholas/ocp/streams/CollectRecue.java
+++ src/com/nicholas/ocp/streams/CollectRecue.java
@@ -1,5 +1,10 @@
 package com.nicholas.ocp.streams;
 
+import java.util.DoubleSummaryStatistics;
+import java.util.IntSummaryStatistics;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
 import java.util.stream.Stream;
 
 public class CollectRecue {
@@ -12,5 +17,21 @@ public class CollectRecue {
 		Stream<String> streamCollec = Stream.of("N", "I");
 		StringBuilder t = streamCollec.collect(StringBuilder::new, StringBuilder::append, StringBuilder::append);
 		System.out.println(t);
+		System.out.println();
+		Stream<String> streamTree = Stream.of("N", "I");
+		StringBuilder set = streamTree.collect(StringBuilder::new, StringBuilder::append, StringBuilder::append);
+		System.out.println(set);
+		System.out.println();
+		Stream<String> testeReduce = Stream.of("n", "y", "j");
+		int le = testeReduce.reduce(1, (x, y) -> x + y.length(), (a, b) -> a + b);
+		System.out.println(le);
+		System.out.println();
+		DoubleStream dbStream = DoubleStream.of(10, 20);
+		DoubleSummaryStatistics statiscs = dbStream.summaryStatistics();
+		System.out.println(statiscs.getAverage());
+		System.out.println();
+		var oh = Stream.of(10, 20, 30);
+		IntSummaryStatistics st = oh.collect(Collectors.summarizingInt(x -> x));
+		System.out.println(st.getAverage());
 	}
 }
---
+++ src/com/nicholas/ocp/streams/OCPCollectors.java
@@ -0,0 +1,42 @@
+package com.nicholas.ocp.streams;
+
+import java.util.IntSummaryStatistics;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class OCPCollectors {
+	/*
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		/*
+		 * Mesmo padrao,passamos um collector ao collect e ele executa a regra de
+		 * negocio
+		 */
+		var myStream = Stream.of(10, 20, 30);
+		IntSummaryStatistics summary = myStream.collect(Collectors.summarizingInt(x -> x));
+		System.out.println("count " + summary.getCount());
+		/*
+		 * Vc pode especificar a classe do map, passando um supplier no ultimo parametro
+		 * do toMap(Function,Function,BinaryOperator,Supplier).
+		 * 
+		 * Lembre-se as duas functions sao para dizer ao collector como criar a key,a
+		 * segunda para dizer como criar o value.
+		 * 
+		 * Se houver mais de uma key duplicata no mesmo map,ira ocorrer uma
+		 * exception!Por isso, passe um BinaryOperator(Optional) para juntar essas duas
+		 * values na mesma key. Esse binaryOperator so sera executado se houver keys
+		 * iguais no map.
+		 * 
+		 * Para especificar a classe do Map,passe um constructor reference no ultimo
+		 * parametro(supplier),mas lembre de passar o BinaryOperator antes!
+		 */
+		var myStream2 = Stream.of(10, 10, 20, 30);
+		Map<Integer, Integer> mp = myStream2
+				.collect(Collectors.toMap(x -> x, x -> x, (x, y) -> x + y + 900, TreeMap::new));
+		System.out.println(mp + " class " + mp.getClass());
+	}
+}
