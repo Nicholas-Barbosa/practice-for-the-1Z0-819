29c8db49214123e84f8f3cf2eb9a4682bbdad3e2
Nicholas-Barbosa <nicholas.henrique0604@hotmail.com>
2020-11-12 13:27:57 -0300

    Date commons symbols and formatter

--- src/com/nicholas/ocp/dates/OCPDates.java
+++ src/com/nicholas/ocp/dates/OCPDates.java
@@ -1,8 +1,46 @@
 package com.nicholas.ocp.dates;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+
 public class OCPDates {
 
 	/*
+	 * Classes do java 11 para expressarmos datas, java.time.LocalDate -> data,
+	 * 
+	 * java.time.LocalTime -> hora
+	 * 
+	 * java.time.LocalDateTime -> dia e hora.
+	 * 
+	 * java.time.ZonedDateTime -> dia e hora numa zona especifica.
+	 * 
 	 * 
 	 */
+
+	public static void main(String[] args) {
+		LocalDate now = LocalDate.now();
+		System.out.println(now);
+		System.out.println();
+		var nowTime = LocalTime.now();
+		System.out.println(nowTime);
+		System.out.println("----OF Method----");
+		var dataOf = LocalDateTime.of(2020, Month.NOVEMBER, 11, 23, 16);
+		System.out.println(dataOf);
+		/*
+		 * Java usa o T para separar a data com a hora ao converter para string
+		 */
+		System.out.println("----DateTimeFormatter----");
+		System.out.println(dataOf.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+		/*
+		 * System.out.println(now.format(DateTimeFormatter.ISO_LOCAL_TIME));
+		 * 
+		 * Exception, tipos diferentes da problema. Estamos convertendo um localDate num
+		 * localTimefi
+		 */
+		System.out.println(dataOf.format(DateTimeFormatter.ofPattern("dd 'de' MMM 'de' YYYY 'AS(T)' hh 'e' mm 'munutos'")));
+		
+	}
 }
---
+++ src/com/nicholas/ocp/dates/OCPDatesSymbolsFormatter.java
@@ -0,0 +1,57 @@
+package com.nicholas.ocp.dates;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class OCPDatesSymbolsFormatter {
+
+	/*
+	 * Simbolos comuns ao trabalhar com datas
+	 * 
+	 * Y -> ano
+	 * 
+	 * M->mes
+	 * 
+	 * d->dia
+	 * 
+	 * h->hora
+	 * 
+	 * m->miunutos
+	 * 
+	 * s->segundos
+	 * 
+	 * a->am/pm
+	 * 
+	 * z-> time zone name
+	 * 
+	 * Z-> time zone offset.
+	 * 
+	 * Alguns simbolos sao aplicados unicamenta a algumas classes.
+	 * 
+	 * LocalDate -> Y,m e d
+	 * 
+	 * LocalTime ->h,m,s
+	 * 
+	 * LocalDateTime -> y,d,M,h,m,s
+	 * 
+	 * ZonedDateTime -> y,M,d,h,m,s,z,Z
+	 * 
+	 * As classes com data/time, possuem um metodo chamado format(DateTimeFormatter)
+	 * e que pega um formatter, enquanto o formatter tbm possui o metodo format que
+	 * pega uma local
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		var data = LocalDate.now();
+		var formatter = DateTimeFormatter.ofPattern("d 'de' MMM 'de' YYYY");
+		System.out.println("com .format " + data.format(formatter) + " formatter.format() " + formatter.format(data));
+		System.out.println();
+		/*
+		 * Lembre-se de usar simbolos suportados por cada classe, o seguinte nao
+		 * executa.Estamos usando m no lugar de M.
+		 */
+		var formatterErrado = DateTimeFormatter.ofPattern("d 'de' m 'de' YYYY");
+		System.out.println(formatterErrado.format(data));
+	}
+}
