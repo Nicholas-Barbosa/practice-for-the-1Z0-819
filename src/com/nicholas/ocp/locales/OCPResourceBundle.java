b38bb91619ea587342bb4cd424aaef390df8ec96
Nicholas-Barbosa <nicholas.henrique0604@hotmail.com>
2020-11-16 12:21:30 -0300

    Bundles for internationalization

---
+++ src/com/nicholas/ocp/locales/OCPResourceBundle.java
@@ -0,0 +1,17 @@
+package com.nicholas.ocp.locales;
+
+public class OCPResourceBundle {
+
+	/*
+	 * Resource bundle contem um objeto locale-specific, usado pelo nosso programa.
+	 * Como se fosse um map, key/value.
+	 * 
+	 * Resource Bundle e comunmente armeazenado num properties file. Properties file
+	 * e um arquivo de formato especifico, que contem key/values pairs.
+	 * 
+	 * Properties file leva o mesmo conceito de key/value, onde o value e separado
+	 * da key, por : ou =
+	 * 
+	 * Podemos criar um resource bundle como uma classe, extendendo ResourceBundle.
+	 */
+}
