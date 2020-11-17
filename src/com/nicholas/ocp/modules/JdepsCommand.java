f0a46f9153e972a19ee00c50cf7c195fe2a175f0
Nicholas-Barbosa <nicholas.henrique0604@hotmail.com>
2020-11-16 23:35:25 -0300

    Jdeps command, java(-d,--show-module-resolution,--list-modules) and jar for modules(-d)

--- src/com/nicholas/ocp/modules/JdepsCommand.java
+++ src/com/nicholas/ocp/modules/JdepsCommand.java
@@ -5,7 +5,17 @@ public class JdepsCommand {
 	/*
 	 * Usamos o jdeps, para mostrar as dependencias do aplicativo.
 	 * 
-	 * java -jdeps arq.jar
+	 * java -jdeps arq.jar -> ira mostar todos os modulos usados, pacotes dos
+	 * modulos que estao sendo usados e os pacotes da nossa aplicacao com seus
+	 * respectivos imports.
 	 * 
+	 * 
+	 * java -jdeps arq.jar -s ou --summary -> ira mostar somente os modulos usados e
+	 * nao os pacotes.
+	 * 
+	 * comandos como java, possuem ferramentes para descrever um modulo(-d ou
+	 * --describe-module), listar modulos disponiveis(--list-modules), resolucao
+	 * logica(--show-module-resolution). Com comando jar, tambem podemos descrever
+	 * um modulo, usando jar -f myu.jar -d ou --describe-module
 	 */
 }
